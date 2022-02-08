57c29f391c6f6aab67bfeff0c3a6a96cbde71bef
Michael S"exit()quit()git config --global user.name Michael <michael.sosu@amalitech.org>
2022-02-08 03:23:16 +0000

    Solution

---
+++ income.py
@@ -0,0 +1,31 @@
+def warn(*args, **kwargs):
+    pass
+import warnings
+warnings.warn = warn
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn import tree
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
+income_data = pd.read_csv('income.csv', header = 0, delimiter = ", ")
+print(income_data.head())
+print(income_data.iloc[0]) #income
+
+income_data["sex-int"] = income_data["sex"].apply(lambda row: 0 if row == "Male" else 1)
+income_data['country-int'] = income_data['native-country'].apply(lambda row:0 if row == "United-states" else 0)
+labels = income_data[['income']]
+data = income_data[["age", "capital-gain", "capital-loss", "hours-per-week",'sex-int', 'country-int']]
+train_data, test_data, train_labels, test_labels = train_test_split(data, labels, random_state = 1)
+
+forest = RandomForestClassifier(random_state = 1)
+forest.fit(train_data, train_labels)
+
+print(forest.score(test_data, test_labels))
+#print(income_data["native-country"].value_counts())
+
+# Tree  Decision
+tree = DecisionTreeClassifier()
+tree.fit(train_data, train_labels)
+print(tree.score(test_data, test_labels))
+print(tree.feature_importances_)
